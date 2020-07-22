46a3a5ffd9b6dde7ec878f6cb0198aa598f1029f
sangbeomLee <wing951@naver.com>
2020-07-22 17:49:52 +0900

    Initial Commit

---
+++ CommnetOnly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CommnetOnly
+//
+//  Created by 이상범 on 2020/07/22.
+//  Copyright © 2020 sangbeomLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
